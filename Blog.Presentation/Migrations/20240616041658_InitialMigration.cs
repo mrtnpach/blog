419c639eb1481d41fdb91ab5cdfb5fd2a0916bb6
Martin Pacheco <martinpacheco@alu.frt.utn.edu.ar>
2024-06-16 01:18:18 -0300

    Migration script and connection string

---
+++ Blog.Presentation/Migrations/20240616041658_InitialMigration.cs
@@ -0,0 +1,152 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Blog.Presentation.Migrations
+{
+    public partial class InitialMigration : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Class",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Year = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Class", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Colleges",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    University = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Faculty = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Colleges", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Students",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Legajo = table.Column<int>(type: "int", nullable: false),
+                    FirstName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    LastName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    EmailAddress = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    CollegeId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Students", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Students_Colleges_CollegeId",
+                        column: x => x.CollegeId,
+                        principalTable: "Colleges",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Assignments",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Date = table.Column<DateTime>(type: "datetime2", nullable: false),
+                    Filename = table.Column<string>(type: "nvarchar(max)", nullable: true),
+                    StudentId = table.Column<int>(type: "int", nullable: false),
+                    ClassId = table.Column<int>(type: "int", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Assignments", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_Assignments_Class_ClassId",
+                        column: x => x.ClassId,
+                        principalTable: "Class",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Assignments_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "InfoItems",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Title = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Description = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    StudentId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_InfoItems", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_InfoItems_Students_StudentId",
+                        column: x => x.StudentId,
+                        principalTable: "Students",
+                        principalColumn: "Id");
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Assignments_ClassId",
+                table: "Assignments",
+                column: "ClassId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Assignments_StudentId",
+                table: "Assignments",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_InfoItems_StudentId",
+                table: "InfoItems",
+                column: "StudentId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Students_CollegeId",
+                table: "Students",
+                column: "CollegeId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Assignments");
+
+            migrationBuilder.DropTable(
+                name: "InfoItems");
+
+            migrationBuilder.DropTable(
+                name: "Class");
+
+            migrationBuilder.DropTable(
+                name: "Students");
+
+            migrationBuilder.DropTable(
+                name: "Colleges");
+        }
+    }
+}
---
+++ MigrationScripts/yl2qvyj0.sql
@@ -0,0 +1,84 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+CREATE TABLE [Class] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    [Year] int NOT NULL,
+    CONSTRAINT [PK_Class] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Colleges] (
+    [Id] int NOT NULL IDENTITY,
+    [University] nvarchar(max) NOT NULL,
+    [Faculty] nvarchar(max) NOT NULL,
+    CONSTRAINT [PK_Colleges] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [Students] (
+    [Id] int NOT NULL IDENTITY,
+    [Legajo] int NOT NULL,
+    [FirstName] nvarchar(max) NOT NULL,
+    [LastName] nvarchar(max) NOT NULL,
+    [EmailAddress] nvarchar(max) NOT NULL,
+    [CollegeId] int NOT NULL,
+    CONSTRAINT [PK_Students] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Students_Colleges_CollegeId] FOREIGN KEY ([CollegeId]) REFERENCES [Colleges] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE TABLE [Assignments] (
+    [Id] int NOT NULL IDENTITY,
+    [Name] nvarchar(max) NOT NULL,
+    [Description] nvarchar(max) NOT NULL,
+    [Date] datetime2 NOT NULL,
+    [Filename] nvarchar(max) NULL,
+    [StudentId] int NOT NULL,
+    [ClassId] int NOT NULL,
+    CONSTRAINT [PK_Assignments] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Assignments_Class_ClassId] FOREIGN KEY ([ClassId]) REFERENCES [Class] ([Id]) ON DELETE CASCADE,
+    CONSTRAINT [FK_Assignments_Students_StudentId] FOREIGN KEY ([StudentId]) REFERENCES [Students] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE TABLE [InfoItems] (
+    [Id] int NOT NULL IDENTITY,
+    [Title] nvarchar(max) NOT NULL,
+    [Description] nvarchar(max) NOT NULL,
+    [StudentId] int NULL,
+    CONSTRAINT [PK_InfoItems] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_InfoItems_Students_StudentId] FOREIGN KEY ([StudentId]) REFERENCES [Students] ([Id])
+);
+GO
+
+CREATE INDEX [IX_Assignments_ClassId] ON [Assignments] ([ClassId]);
+GO
+
+CREATE INDEX [IX_Assignments_StudentId] ON [Assignments] ([StudentId]);
+GO
+
+CREATE INDEX [IX_InfoItems_StudentId] ON [InfoItems] ([StudentId]);
+GO
+
+CREATE INDEX [IX_Students_CollegeId] ON [Students] ([CollegeId]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20240616041658_InitialMigration', N'6.0.31');
+GO
+
+COMMIT;
+GO
+
